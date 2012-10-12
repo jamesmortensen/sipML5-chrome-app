e176a23298655a30ac31b921872ebf72484775e7
James Mortensen <james.mortensen@a-cti.com>
2012-10-12 10:43:24 -0700

    Added if block to handle loading app using experimental API's in Chrome Stable 22

--- main.js
+++ main.js
@@ -4,7 +4,17 @@
  * @see http://developer.chrome.com/trunk/apps/app.runtime.html
  * @see http://developer.chrome.com/trunk/apps/app.window.html
  */
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('mainpage.html',
-    {width: 1190, height: 709});
-});
+if(chrome.experimental.app.onLaunched) {
+  // Chrome v22
+  chrome.experimental.app.onLaunched.addListener(function() {
+    chrome.app.window.create('mainpage.html',
+      {width: 1190, height: 709});
+  });
+
+} else {
+  // Chrome v24
+  chrome.app.runtime.onLaunched.addListener(function() {
+    chrome.app.window.create('mainpage.html',
+      {width: 1190, height: 709});
+  });
+}
