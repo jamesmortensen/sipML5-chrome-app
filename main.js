3b968bff8e5da748e12d5f2c2c449f514cb59f25
James Mortensen <james.mortensen@a-cti.com>
2015-10-16 14:10:58 +0530

    Eliminated unneeded code

--- main.js
+++ main.js
@@ -4,17 +4,10 @@
  * @see http://developer.chrome.com/trunk/apps/app.runtime.html
  * @see http://developer.chrome.com/trunk/apps/app.window.html
  */
-if(chrome.experimental.app.onLaunched) {
-  // Chrome v22
-  chrome.experimental.app.onLaunched.addListener(function() {
-    chrome.app.window.create('mainpage.html',
-      {width: 1190, height: 709});
-  });
 
-} else {
-  // Chrome v24
-  chrome.app.runtime.onLaunched.addListener(function() {
-    chrome.app.window.create('mainpage.html',
-      {width: 1190, height: 709});
-  });
-}
+// Chrome v24
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('mainpage.html',
+    {width: 1190, height: 709});
+});
+
